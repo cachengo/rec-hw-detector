088251a87c3df23d33c80cc46687cb569be23c8d
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-11-25 16:55:59 -0400

    Add hw type for Ampere server

---
+++ src/hw_detector/hw_types/hw_ampere.py
@@ -0,0 +1,29 @@
+# Copyright 2019 Nokia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from hw_detector.hw_type import HWType
+
+class HWDELL(HWType):
+    def __init__(self):
+        super(HWDELL, self).__init__()
+        self.matches = {'Board Product' : 'FALCON'}
+        self.hwtype = 'AMPERE'
+        self.vendor_name = 'AMPERE'
+        self.productfamily = 'FALCON'
+        self.disk_map = {'os': '/dev/sda',
+                         'osd' : ['/dev/sdb',
+                                  '/dev/sdc',
+                                  '/dev/sdd'
+                                 ]
+                        }
